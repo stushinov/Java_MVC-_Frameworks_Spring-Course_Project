7ad8c6cd5c8a396d8013e9b80851c1228d6bba5e
Stoyan Tushinov <tanuka@mail.bg>
2019-03-22 12:48:13 +0200

    Added MySql connector J and jdbc configuration.

--- AuthenticationService/src/main/java/org/tushinov/auth/ServiceStarter.java
+++ AuthenticationService/src/main/java/org/tushinov/auth/ServiceStarter.java
@@ -3,6 +3,9 @@ package org.tushinov.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * TODO: Added branch "auth_development", remove before merging to master
+ */
 @SpringBootApplication
 public class ServiceStarter {
     public static void main(String[] args) {
