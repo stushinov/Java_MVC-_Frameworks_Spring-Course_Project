6eb661d8cb329c90b4ea719b0d34cd8a60a375da
Stoyan Tushinov <tanuka@mail.bg>
2019-03-22 12:57:33 +0200

    Added 'WebSecurityConfiguration' class.

---
+++ AuthenticationService/src/main/java/org/tushinov/auth/config/web/WebSecurityConfiguration.java
@@ -0,0 +1,15 @@
+package org.tushinov.auth.config.web;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().disable()
+                .authorizeRequests().anyRequest().permitAll();
+    }
+}
